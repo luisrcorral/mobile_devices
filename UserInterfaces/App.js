b09823f68e86fb9c6cf54722355b92b61f9fe020
Maglo22 <bruno_mg96@hotmail.com>
2019-02-21 11:48:20 -0600

    User Interfaces example with two screens and a text input

--- UserInterfaces/App.js
+++ UserInterfaces/App.js
@@ -1,55 +1,91 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+// Home screen
 class HomeScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { inputText: '' }; // State that holds text input value
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View style={ styles.container }>
+        <Text style={ styles.text }>Home Screen</Text>
+        <TextInput
+          style={ styles.text_input }
+          placeholder = "Enter text here"
+          onChangeText = { data => this.setState({ inputText: data })} // Set state to the value entered as input
+        />
         <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          title="Go to Second screen"
+          onPress={() => {
+              this.props.navigation.navigate('Second', { // Go to specified screen
+              inputText: this.state.inputText, // Pass this parameter (text input) when navigating
+            });
+          }}
         />
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
+// Second screen
+class SecondScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
+    const inputText = navigation.getParam('inputText', 'Placeholder'); // Name of parameter expected to receive and a default value if not available
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
+      <View style={ styles.container }>
+        <Text style={ styles.text }>Second Screen</Text>
+        <Text>Text Input from Home screen: { JSON.stringify(inputText) }</Text>
         <Button
           title="Go to Home"
           onPress={() => this.props.navigation.navigate('Home')}
         />
         <Button
           title="Go back"
-          onPress={() => this.props.navigation.goBack()}
+          onPress={() => this.props.navigation.goBack()} // Goes back to previous screen
         />
       </View>
     );
   }
 }
 
-const RootStack = createStackNavigator(
+// Stack of screens
+const MainStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Second: SecondScreen,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home', // Initial screen
   }
 );
 
-const AppContainer = createAppContainer(RootStack);
 
+//Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  text_input:{
+    textAlign: 'center',
+    fontSize: 16,
+  }
+});
+
+const AppContainer = createAppContainer(MainStack);
+
+// Renders the stack navigator
 export default class App extends React.Component {
   render() {
     return <AppContainer />;
